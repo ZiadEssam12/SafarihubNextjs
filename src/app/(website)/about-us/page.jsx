5db4f53971974dccfdcbdeca391ab9a6624aa5c1
ziad <ziadessam1287@gmail.com>
2025-03-03 15:22:34 +0200

    finished a section in about us page

--- src/app/(website)/about-us/page.jsx
+++ src/app/(website)/about-us/page.jsx
@@ -1,4 +1,9 @@
-import { LucideAward, LucideClock, LucideLeaf } from "@/icons/Icons";
+import {
+  LucideAward,
+  LucideClock,
+  LucideLeaf,
+  SolarPhoneCallingOutline,
+} from "@/icons/Icons";
 import Image from "next/image";
 
 export const metadata = {
@@ -18,6 +23,7 @@ export default function page() {
             alt="Egyptian Pyramids"
             className="object-cover brightness-75"
             fill
+            priority
           />
           <div className="absolute inset-0 bg-black/40 flex flex-col items-center justify-center text-white p-4">
             <h1 className="text-4xl md:text-6xl font-bold mb-4 text-center ">
@@ -82,13 +88,20 @@ export default function page() {
 
             {/* tabs */}
 
-            <div className="mt-12 bg-secondary p-8 rounded-lg text-center">
+            <div className="mt-12 bg-gray-100 p-8 rounded-lg text-center">
               <h3 className="text-2xl font-bold mb-4">Not Sure Where to Go?</h3>
               <p className="text-lg mb-6">
                 Our experts will help you craft the perfect Egyptian adventure
               </p>
-              {/* call us */}
-              <div className="flex flex-col sm:flex-row justify-center items-center gap-4"></div>
+              <div className="flex flex-col sm:flex-row justify-center items-center gap-4">
+                <button className="flex justify-center items-center gap-2 bg-black hover:bg-black/90 transition-all duration-150 py-2 px-8 text-white rounded-md">
+                  <SolarPhoneCallingOutline className="h-4 w-4" />
+                  Call Us: 01001264478
+                </button>
+                <button className="bg-white px-5 py-2 rounded-md border-[1px] border-gray-300 hover:bg-transparent transition-all duration-150">
+                  Let&apos;s Chat
+                </button>
+              </div>
             </div>
           </section>
 
