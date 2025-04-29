c65bfa59fbe0897cf93b0e451ac02cbb149f5cfb
ziad <ziadessam1287@gmail.com>
2025-04-29 05:01:53 +0300

    reinstalled all packages

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -257,11 +257,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:postgres@localhost:5432/safarihub?schema=public"
+        "value": null
       }
     }
   },
