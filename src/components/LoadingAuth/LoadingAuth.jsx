3fe90ab64ad7290ebc6ff3cafa4d33c1132da997
ziad <ziadessam1287@gmail.com>
2025-05-03 14:26:46 +0300

    added a high priority for loading image

--- src/components/LoadingAuth/LoadingAuth.jsx
+++ src/components/LoadingAuth/LoadingAuth.jsx
@@ -4,6 +4,9 @@ import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Logo from "../Logo/Logo";
 
+// Preload the image
+const logoSrc = "/orangeLogo-removebg-preview.webp";
+
 export default function LoadingAuth({ children }) {
   const { status } = useSession();
 
@@ -12,13 +15,17 @@ export default function LoadingAuth({ children }) {
       <div className="fixed top-0 left-0 size-full z-50 flex flex-col justify-between items-center py-20 bg-gray-100 dark:bg-gray-800/50">
         <div className="flex-1 flex items-center justify-center">
           <Image
-            src={"/orangeLogo-removebg-preview.webp"}
+            src={logoSrc}
             width={150}
             height={150}
             alt="Safari Hub Logo"
+            priority
+            loading="eager"
+            fetchPriority="high"
+            unoptimized
           />
         </div>
-        <div className="w-full flex justify-center pb-10">
+        <div className="w-full flex justify-center">
           <Logo />
         </div>
       </div>
