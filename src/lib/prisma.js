f9b374fb1b1a68cba991936a6f7cd9c32f146f36
ziad <ziadessam1287@gmail.com>
2025-05-07 12:32:10 +0300

    checking prisma error

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,9 +1,7 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global;
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
 export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
